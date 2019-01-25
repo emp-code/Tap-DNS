cec455a7fef3136c4bd5e0ee3f96395fd321b998
EMPcode <45713960+emp-code@users.noreply.github.com>
2019-01-25 22:29:49 +0200

    Cleanup

--- protocol.h
+++ protocol.h
@@ -1,12 +1,10 @@
 #ifndef TAPDNS_PROTOCOL_H
 #define TAPDNS_PROTOCOL_H
 
-#include <stddef.h>
-
 int dnsCreateAnswer(char* buffer, const char* req, const int ip);
 int dnsCreateRequest(char rq[100], const char* domain, const size_t domainLen);
 size_t dnsRequest_GetDomain(const char* req, char* holder);
-int dnsResponse_GetIp(const int offset, const char* res, const int resLen);
 int dnsRequest_GetOpcode(const char* req);
+int dnsResponse_GetIp(const int offset, const char* res, const int resLen);
 
 #endif
