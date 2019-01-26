6ab276a8984ac340c205bf8245721bf44d613185
EMPcode <45713960+emp-code@users.noreply.github.com>
2019-01-26 09:55:33 +0200

    Minor cleanup

--- database.c
+++ database.c
@@ -18,11 +18,7 @@ int dbGetIp(const char* domain, const size_t lenDomain) {
 	sqlite3_bind_text(query, 1, domain, lenDomain, SQLITE_STATIC);
 	ret = sqlite3_step(query);
 
-	int result = 0;
-
-	if (ret == SQLITE_ROW) {
-		result  = sqlite3_column_int(query, 0);
-	}
+	const int result = (ret == SQLITE_ROW) ? sqlite3_column_int(query, 0) : 0;
 
 	sqlite3_finalize(query);
 	sqlite3_close_v2(db);
@@ -40,7 +36,7 @@ int dbSetIp(const char* domain, const size_t lenDomain, const int ip) {
 	if (ret != SQLITE_OK) {printf("ERROR: Failed to prepare SQL insert query: %d\n", ret); sqlite3_close_v2(db); return -2;}
 
 	sqlite3_bind_text(query, 1, domain, lenDomain, SQLITE_STATIC);
-	sqlite3_bind_int(query, 2, (int)ip);
+	sqlite3_bind_int(query, 2, ip);
 	ret = sqlite3_step(query);
 
 	if (ret == SQLITE_DONE) {
@@ -53,7 +49,7 @@ int dbSetIp(const char* domain, const size_t lenDomain, const int ip) {
 	ret = sqlite3_prepare_v2(db, "UPDATE dns SET ip = ?, ts = STRFTIME('%s', 'NOW') WHERE domain = ?", 66, &query, NULL);
 	if (ret != SQLITE_OK) {printf("ERROR: Failed to prepare SQL query: %d\n", ret); sqlite3_close_v2(db); return -3;}
 
-	sqlite3_bind_int(query, 1, (int)ip);
+	sqlite3_bind_int(query, 1, ip);
 	sqlite3_bind_text(query, 2, domain, lenDomain, SQLITE_STATIC);
 	ret = sqlite3_step(query);
 
