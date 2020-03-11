6b897ba6f17161618098daa25b056cd97cbf1e1d
EMPcode <dev@empost.eu>
2020-03-11 03:47:17 +0200

    Cleanup

--- Includes/bit.c
+++ Includes/bit.c
@@ -1,16 +1,16 @@
 // bit.c: Get or set a specific bit in a byte
 
 // Get bit #1-8 from a byte
-int getBit(const char* byte, const int pos) {
-	const unsigned char tmp = *(byte + ((pos - 1) / 8));
-	const unsigned char mask = 1 << (8 - pos);
+int getBit(const char *byte, const int bit) {
+	const unsigned char tmp = *(byte + ((bit - 1) / 8));
+	const unsigned char mask = 1 << (8 - bit);
 	return (tmp & mask) > 0;
 }
 
 // Set bit #1-8 on a byte
-void setBit(char* byte, const int pos, const int value) {
-	unsigned char* pTmp = (unsigned char*)byte + (pos - 1) / 8;
-	const unsigned char mask = 1 << (8 - pos);
+void setBit(char *byte, const int bit, const int value) {
+	unsigned char *pTmp = (unsigned char*)byte + (bit - 1) / 8;
+	const unsigned char mask = 1 << (8 - bit);
 
 	if (value > 0)
 		*pTmp |= mask;
