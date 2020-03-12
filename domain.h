c6616010c61aa771a1f74bb23fd9d39b86f002d8
EMPcode <dev@empost.eu>
2020-03-12 08:03:23 +0200

    Maxlen to 256

--- domain.h
+++ domain.h
@@ -1,7 +1,7 @@
 #ifndef TAPDNS_DOMAIN_H
 #define TAPDNS_DOMAIN_H
 
-#define TAPDNS_MAXLEN_DOMAIN 200
+#define TAPDNS_MAXLEN_DOMAIN 256
 
 int isInvalidDomain(const char * const domain, const size_t domainLen);
 
