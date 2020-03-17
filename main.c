4045245d1e4727b22a11b1bc647da136b81538b1
EMPcode <dev@empost.eu>
2020-03-17 15:41:01 +0200

    Remove forking on accept()

--- main.c
+++ main.c
@@ -28,37 +28,18 @@ int initSocket(const int sock) {
 }
 
 void acceptConnections_tcp() {
-	// Create a TCP socket to accept connections on
 	const int sock = socket(AF_INET, SOCK_STREAM, 0);
 	if (sock < 0) {puts("ERROR: Failed opening TCP socket"); return;}
-
-	// Init the socket
 	if (initSocket(sock) != 0) {puts("ERROR: Failed binding TCP socket"); return;}
 
-	// Accept connections on the socket
 	while(1) {
 		const int newSock = accept(sock, NULL, NULL);
 		if (newSock < 0) {puts("ERROR: Failed accepting connection"); return;}
 
-		const int pid = fork();
-		if (pid < 0) {puts("ERROR: Failed forking"); return;}
-
-		if (pid == 0) {
-			// Child thread: Respond to the client
-			close(sock);
-
-			// Read the request from the client
-			unsigned char req[TAPDNS_BUFLEN + 1];
-			const int reqLen = recv(newSock, req, TAPDNS_BUFLEN, 0);
-			if (reqLen < 0) {perror("Failed receiving a connection"); close(newSock); return;}
-
-			respond(newSock, req, reqLen, NULL, 0);
-
-			close(newSock);
-			return;
-		}
-
-		// Parent thread: Continue loop to accept new clients
+		unsigned char req[TAPDNS_BUFLEN + 1];
+		const int reqLen = recv(newSock, req, TAPDNS_BUFLEN, 0);
+		if (reqLen < 0) {perror("Failed receiving a connection"); close(newSock); return;}
+		respond(newSock, req, reqLen, NULL, 0);
 		close(newSock);
 	}
 
@@ -66,28 +47,17 @@ void acceptConnections_tcp() {
 }
 
 void acceptConnections_udp() {
-	// Create a UDP socket to accept connections on
 	const int sock = socket(AF_INET, SOCK_DGRAM, 0);
 	if (sock < 0) {puts("ERROR: Failed opening UDP socket"); return;}
-
-	// Init the socket
 	if (initSocket(sock) != 0) {puts("ERROR: Failed binding UDP socket"); return;}
 
-	// Accept connections on the socket
 	while(1) {
-		struct sockaddr_in addrIn; // Client address
+		struct sockaddr_in addrIn;
 		socklen_t addrlen = sizeof(addrIn);
-
-		unsigned char req[TAPDNS_BUFLEN]; // Request holder
+		unsigned char req[TAPDNS_BUFLEN];
 		const int reqLen = recvfrom(sock, req, TAPDNS_BUFLEN, 0, (struct sockaddr*)&addrIn, &addrlen);
 		if (reqLen < 0) {perror("ERROR: Failed receiving a connection"); continue;}
-
-		const int pid = fork();
-		if (pid < 0) {puts("ERROR: Failed forking connection"); return;}
-		else if (pid != 0) continue; // 0 = Child
-
 		respond(sock, req, reqLen, (struct sockaddr*)&addrIn, addrlen);
-		return;
 	}
 
 	close(sock);
@@ -102,7 +72,7 @@ int main() {
 
 	puts(">>> Tap:DNS - The Attenuating Proxy: Deadbolt Name Service");
 
-	// Fork once here to accept both UDP and TCP connections
+	// Fork to accept both UDP and TCP connections
 	const int pid = fork();
 	if (pid < 0) {puts("ERROR: Failed forking connection"); return 1;}
 
