d4aedde6baf11e4fbb97a0b8f3dc99e8bdf1b917
EMPcode <dev@empost.eu>
2020-10-27 20:45:13 +0200

    No underscore in hostnames

--- domain.c
+++ domain.c
@@ -11,7 +11,7 @@ bool isDomainValid(const char * const domain, const size_t domainLen) {
 
 	if (domainLen < 4 || domainLen > TAPDNS_MAXLEN_DOMAIN) return 2;
 
-	if (strspn(domain, "abcdefghijklmnopqrstuvwxyz0123456789.-_") != domainLen) return 3; // Only letters, numbers, ./-/_ allowed
+	if (strspn(domain, "abcdefghijklmnopqrstuvwxyz0123456789.-") != domainLen) return 3; // Allow only letters/numbers/hyphen/dot
 
 	if (!isalnum(domain[0])) return 4; // First character must be alphanumeric
 
